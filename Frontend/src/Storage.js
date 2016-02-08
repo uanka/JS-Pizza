31ac681b63f407faa6a4ddf3f8dbe330ca36111a
uanka <ankazeml97@gmail.com>
2016-02-09 01:31:21 +0200

    Frontend
    html&css, without pizza's data

---
+++ Frontend/src/Storage.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Земляничка on 03.02.2016.
+ */
+var basil	=	require('basil.js');
+basil	=	new	basil();
+exports.get =	function(key)	{
+    return	basil.get(key);
+};
+exports.set =	function(key,	value)	{
+    return	basil.set(key,	value);
+};
